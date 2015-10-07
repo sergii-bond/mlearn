2f4e3e56e2afd6eb566107a93fc98be276cf22c5
sergii-bondariev <SergeyBondarev@gmail.com>
2015-10-07 16:48:25 -0700

    init

---
+++ course/nbayes/studentMain.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+""" Complete the code in ClassifyNB.py with the sklearn
+    Naive Bayes classifier to classify the terrain data.
+    
+    The objective of this exercise is to recreate the decision 
+    boundary found in the lesson video, and make a plot that
+    visually shows the decision boundary """
+
+
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture, output_image
+from ClassifyNB import classify
+
+import numpy as np
+import pylab as pl
+
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+### the training data (features_train, labels_train) have both "fast" and "slow" points mixed
+### in together--separate them so we can give them different colors in the scatterplot,
+### and visually identify them
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+
+clf = classify(features_train, labels_train)
+
+
+    ### draw the decision boundary with the text points overlaid
+prettyPicture(clf, features_test, labels_test)
+output_image("test.png", "png", open("test.png", "rb").read())
+
+
+# Accuracy
+# method 1
+pred = clf.predict(features_test)
+accuracy = sum(labels_test == pred) / float(len(labels_test))
+print("Accuracy is: ", accuracy)
+
+# method 2
+from sklearn.metrics import accuracy_score
+print("Accuracy is: ", accuracy_score(pred, labels_test))
+
+# method 3
+print("Accuracy is: ", clf.score(features_test, labels_test))
+
+
+
+
+
+
+
